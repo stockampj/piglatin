829bf85938662c09faea708172a12d696ba45e7b
Joel Stockamp and Jaime Takikawa <jaime@>
2019-08-21 10:37:32 -0700

    add function for consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,40 @@
 $(document).ready(function() {
-  alert("hello");
   $("form#userInput").submit(function(event) {
     event.preventDefault();
 
-    var userInput = "early";
+    var userInput = "arly";
+    var characters = [];
+    var vowels = ["a", "e", "i", "o", "u", "y"];
+
     function vowelWord(word) {
       return (word + "ay");
     }
 
-    function consonantWord(word) {
-      var characters = [];
-      characters = word.slice();
-      console.log(characters)
+    function consonantWord(word, characters) {
+      characters = word.split('');
+      var firstLetter = word.slice(0, 1);
+      var truncated = word.slice(1);
+      return truncated + firstLetter + "ay";
     }
-    var output = vowelWord(userInput);
+
+    function wordCruncher(word) {
+      characters = word.split('');
+      var firstLetter = characters[0];
+      var vowelCheck = vowels.indexOf(firstLetter);
+      if (vowelCheck > 0) {
+      console.log(vowelCheck);
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    var output = wordCruncher(userInput);
     console.log(output);
   });
 });
+
+
+// ("a" || "e" || "i" || "o" || "u" || "y")
+//
+// ((characters[0] !== "a") || (characters[0] !== "e") || (characters[0] !== "i") || (characters[0] !== "o") || (characters[0] !== "u") || (characters[0] !== "y"))
